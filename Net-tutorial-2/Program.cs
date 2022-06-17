bf19399e6912f349b946e9ce45c0b05d9c1e2159
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-17 12:01:25 -0600

    added overloaded Console.WriteLine methods

--- Net-tutorial-2/Program.cs
+++ Net-tutorial-2/Program.cs
@@ -115,3 +115,10 @@
 Random dice = new Random();
 int roll = dice.Next(1, 7);
 Console.WriteLine(roll);
+
+int number = 7;
+string text = "seven";
+
+Console.WriteLine(number);
+Console.WriteLine();
+Console.WriteLine(text);
