ea450cdba83baca361c7485d68c8f9cc9dd1c6d3
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-17 12:26:15 -0600

    added overloaded Next method

--- Net-tutorial-2/Program.cs
+++ Net-tutorial-2/Program.cs
@@ -112,9 +112,9 @@
 // Console.WriteLine($"The temperature is {celsius} celsius");
 
 // Methods in .NET library
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
 
 int number = 7;
 string text = "seven";
@@ -122,3 +122,12 @@ string text = "seven";
 Console.WriteLine(number);
 Console.WriteLine();
 Console.WriteLine(text);
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"first roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
