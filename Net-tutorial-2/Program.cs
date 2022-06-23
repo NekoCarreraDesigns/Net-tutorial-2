10d497e3ada62dfa39e191470d3c990d3fa11f2d
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-23 11:30:15 -0600

    added reference types

--- Net-tutorial-2/Program.cs
+++ Net-tutorial-2/Program.cs
@@ -351,3 +351,8 @@
 // Console.WriteLine($"float : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 // Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 // Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+// reference types 
+int[] data = new int[3];
+string shortenedString = "Hello World!";
+Console.WriteLine(shortenedString);
