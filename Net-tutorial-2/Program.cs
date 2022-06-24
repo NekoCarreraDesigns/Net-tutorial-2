a7ec4d7d0456131c99a2262b9c5a2e911c3222f7
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-24 13:51:07 -0600

    commented out previous code

--- Net-tutorial-2/Program.cs
+++ Net-tutorial-2/Program.cs
@@ -440,35 +440,36 @@
 // }
 
 // composite formatting
-string first = "Hello";
-string second = "World";
+// string first = "Hello";
+// string second = "World";
 // string result = string.Format("{0} {1}!", first, second);
 // using format tokens
 // Console.WriteLine("{1} {0}!", first, second);
 // Console.WriteLine("{0} {0} {0}!", first, second);
+
 // using interpolation
-Console.WriteLine($"{first} {second}!");
-Console.WriteLine($"{second} {first}!");
-Console.WriteLine($"{first} {first} {first}!");
+// Console.WriteLine($"{first} {second}!");
+// Console.WriteLine($"{second} {first}!");
+// Console.WriteLine($"{first} {first} {first}!");
 
-decimal price = 123.45m;
-int discount = 50;
-Console.WriteLine($"Price: {price:C} (Save{discount:C})");
+// decimal price = 123.45m;
+// int discount = 50;
+// Console.WriteLine($"Price: {price:C} (Save{discount:C})");
 
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N4} units");
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N4} units");
 
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P2}");
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P2}");
 
-int invoiceNumber = 1201;
-decimal productMeasurement = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
+// int invoiceNumber = 1201;
+// decimal productMeasurement = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
 
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"   Measurement: {productMeasurement:N3} mg");
-Console.WriteLine($"     Sub total: {subtotal:C}");
-Console.WriteLine($"         Tax percentage:  {taxPercentage:P2}");
-Console.WriteLine($"     Total due: {total:C}");
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Measurement: {productMeasurement:N3} mg");
+// Console.WriteLine($"     Sub total: {subtotal:C}");
+// Console.WriteLine($"         Tax percentage:  {taxPercentage:P2}");
+// Console.WriteLine($"     Total due: {total:C}");
