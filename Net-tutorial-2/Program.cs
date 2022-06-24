d8531a281dd0736a5ff53fe5f9044d2a7dc04e56
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-24 13:46:43 -0600

    added code for string interpolation with formatting

--- Net-tutorial-2/Program.cs
+++ Net-tutorial-2/Program.cs
@@ -450,3 +450,25 @@ string second = "World";
 Console.WriteLine($"{first} {second}!");
 Console.WriteLine($"{second} {first}!");
 Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save{discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+int invoiceNumber = 1201;
+decimal productMeasurement = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Measurement: {productMeasurement:N3} mg");
+Console.WriteLine($"     Sub total: {subtotal:C}");
+Console.WriteLine($"         Tax percentage:  {taxPercentage:P2}");
+Console.WriteLine($"     Total due: {total:C}");
