b64e885daee5c05fc6b53b6e5bfc2640f7cc1f8a
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-24 12:47:44 -0600

    added code to join and  split string

--- Net-tutorial-2/Program.cs
+++ Net-tutorial-2/Program.cs
@@ -429,5 +429,12 @@
 string value = "this is the way";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
-string result = new string(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
 Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
