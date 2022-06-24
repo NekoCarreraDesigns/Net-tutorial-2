0d8db60b6ea1799b43843a7a47a403e62feab706
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-24 12:50:11 -0600

    Commented out previous code

--- Net-tutorial-2/Program.cs
+++ Net-tutorial-2/Program.cs
@@ -426,15 +426,15 @@
 //     Console.WriteLine($"-- {box}");
 // }
 
-string value = "this is the way";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = String.Join(",", valueArray);
-Console.WriteLine(result);
+// string value = "this is the way";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
 
-string[] items = result.Split(',');
-foreach (string item in items)
-{
-    Console.WriteLine(item);
-}
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
